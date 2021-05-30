34605983b7a998c5f85b4968e78ae3d134ae6255
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-30 08:17:16 +0200

    Filter by date and views

--- src/core/admin.py
+++ src/core/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from .models import Author,Category,Journal
 
 
+class JournalAdmin(admin.ModelAdmin):
+    list_display=('title','publish_date','reviewed')
+
 admin.site.register(Author)
 admin.site.register(Category)
-admin.site.register(Journal)
+admin.site.register(Journal ,JournalAdmin)
--- src/core/views.py
+++ src/core/views.py
@@ -1,29 +1,56 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Journal
+from .models import Journal,Category
+
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
 
 def BootstrapFilterView(req):
     qs = Journal.objects.all()
+    categories = Category.objects.all()
     title_contains = req.GET.get('title_contains')
     id_exact = req.GET.get('id_exact')
     title_or_author = req.GET.get('title_or_author')
     view_count_min = req.GET.get('view_count_min')
     view_count_max = req.GET.get('view_count_max')
     date_min = req.GET.get('date_min')
-    date_min = req.GET.get('date_min')
     date_max = req.GET.get('date_max')
     category = req.GET.get('category')
+    reviewed = req.GET.get('reviewed')
+    notReviewed = req.GET.get('notReviewed')
 
-    # Filter the all journals
-    if title_contains != '' and title_contains is not None:
+    # Filter all journals
+    if is_valid_queryparam(title_contains):
         qs = qs.filter(title__icontains=title_contains) 
-    elif id_exact != '' and id_exact is not None:
+    elif is_valid_queryparam(id_exact):
         qs = qs.filter(id__exact = id_exact)
-    elif title_or_author != '' and title_or_author is not None: 
+    elif is_valid_queryparam(title_or_author): 
         qs = qs.filter(
             Q(title__icontains=title_or_author)|
             Q(author__name__icontains=title_or_author)
         ).distinct()
+    
+    if is_valid_queryparam(view_count_min) :
+      qs = qs.filter(views__gte=view_count_min)
+
+    if is_valid_queryparam(view_count_max) :
+        qs.filter(views__lte=view_count_max)
+    
+    if is_valid_queryparam(date_min) :
+      qs = qs.filter(publish_date__gte=date_min)
+
+    if is_valid_queryparam(date_max) :
+       qs = qs.filter(publish_date__lte=date_max)
+
+    if reviewed:
+        qs = qs.filter(reviewed=True)
+    elif notReviewed:
+        qs = qs.exclude(reviewed=True)
+
+    if is_valid_queryparam(category) and category != 'Choose...':
+        qs = qs.filter(categories__name=category)
     return render(req, "bootstrap_form.html" , {
-        'queryset':qs
+        'queryset':qs,
+        'categories': categories
     })
